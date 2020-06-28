f5516e6400ef5d115461814b7ba7ca3c545baf99
bakarcu <66953154+bakarcu@users.noreply.github.com>
2020-06-28 10:36:47 +0600

    Update cachematrix.R
    Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Assignment : Catching the Inverse of a Matrix
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+#Function-01
 
+makeCacheMatrix <- function(x = matrix()){
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() {x}
+  setInverse <- function(inverse) {inv <<- inverse}
+  getInverse <- function() {inv}
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
+#Function-02
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...){
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$get()
+  inv <- solve(mat, ...)
+  x$setInverse(inv)
+  inv
 }
+
+#To check the output
+
+abmatrix <- makeCacheMatrix(matrix(1:4, nrow = 2, ncol = 2))
+abmatrix$get()
+
+abmatrix$getInverse()
+
+#if it returns "NULL" that means the matrix hasn't been cached. So try to get cache data by CacheSolve
+
+cacheSolve(abmatrix)
+
+#Now try to inverse with get inverse function
+
+abmatrix$getInverse()
+
+
